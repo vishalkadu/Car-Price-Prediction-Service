89003d6202fe5bd8fc8ae15ea1c478142f027783
Vishal Kadu <86619476+vishalkadu@users.noreply.github.com>
2022-03-07 01:32:39 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# My2ndCar Price Prediction Service.
+
+The My2ndCar Price Prediction Service App is built using flask web application, Python 3.10,scientific Python Lbraries and essential data(fuel of algorithm) which predicts Selling Price Of Car which is our dependant feature) based on given independent features like Car_Name,	Year,	Selling_Price,	Present_Price,Kms_Driven,	Fuel_Type,	Seller_Type,	Transmission, and Owner. The dataset is available at Kaggle, and it's provided by cardekho.com.
+App is deployed on Heroku cloud(pass).
+
+
+# Tools Used  
+![](https://forthebadge.com/images/badges/made-with-python.svg)
+[<img target="_blank" src="https://flask.palletsprojects.com/en/1.1.x/_images/flask-logo.png" width=170>](https://flask.palletsprojects.com/en/1.1.x/) [<img target="_blank" src="https://number1.co.za/wp-content/uploads/2017/10/gunicorn_logo-300x85.png" width=280>](https://gunicorn.org) [<img target="_blank" src="https://scikit-learn.org/stable/_static/scikit-learn-logo-small.png" width=200>](https://scikit-learn.org/stable/) 
+
+
---
+++ model.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+import pickle
+
+data=pd.read_csv('dataset.csv')
+dataset=data[['Year','Selling_Price','Present_Price','Kms_Driven','Fuel_Type','Seller_Type','Transmission','Owner']]
+dataset['Present_Year']=2020
+dataset['Number_of_Years_Old']=dataset['Present_Year']- dataset['Year']
+dataset.drop(labels=['Year', 'Present_Year'],axis=1,inplace=True)
+Fuel_Type=dataset[['Fuel_Type']]
+Fuel_Type=pd.get_dummies(Fuel_Type, drop_first=True)
+
+Seller_Type=dataset[['Seller_Type']]
+Seller_Type=pd.get_dummies(Seller_Type, drop_first=True)
+
+Transmission=dataset[['Transmission']]
+Transmission=pd.get_dummies(Transmission, drop_first=True)
+
+dataset=pd.concat([dataset,Fuel_Type, Seller_Type, Transmission], axis=1)
+
+dataset.drop(labels=['Fuel_Type', 'Seller_Type', 'Transmission'], axis=1, inplace=True)
+
+sell=dataset['Selling_Price']
+dataset.drop(['Selling_Price'], axis=1, inplace=True)
+dataset=dataset.join(sell)
+
+X=dataset.iloc[:,:-1]
+y=dataset.iloc[:,-1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+model = DecisionTreeRegressor(random_state = 1)
+model.fit(X_train, y_train)
+y_pred=model.predict(X_test)
+
+
+
+pickle.dump(model, open("dtrmodel.pkl", "wb"))
+
+# load model from file
+model = pickle.load(open("dtrmodel.pkl", "rb"))
+
